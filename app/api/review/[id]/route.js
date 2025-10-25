e569875e8b44384ea141a20f117c3db49b6f38bb
Fathismael93 <fathismael@gmail.com>
2025-10-25 22:12:27 +0300

    Implemented all files for rating and comment

--- app/api/review/[id]/route.js
+++ app/api/review/[id]/route.js
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function PUT(req, { params }) {
   try {
     console.log();
-    const body = await req.bdoy();
+    const body = await req.json();
 
     console.log("Body for review", body);
 
