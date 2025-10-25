66a95aa609522e010ab1cfa2cd192c7b6db007b6
Fathismael93 <fathismael@gmail.com>
2025-10-26 00:52:20 +0300

    Created Reviews and added it to ProductDetails

--- components/products/Reviews.jsx
+++ components/products/Reviews.jsx
@@ -1,5 +1,3 @@
-import StarRatings from "react-star-ratings";
-
 const Reviews = ({ reviews }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
