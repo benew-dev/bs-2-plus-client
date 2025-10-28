46a8de39397adf3e2ebde9afed4674a19740f261
Fathismael93 <fathismael@gmail.com>
2025-10-28 12:02:52 +0300

    Updated Hero component

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useMemo, useCallback } from "react";
 import Link from "next/link";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Shield, ShoppingBag, TrendingUp } from "lucide-react";
 import { CldImage } from "next-cloudinary";
 import { useSwipeable } from "react-swipeable";
 
